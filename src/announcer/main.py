63f7bba7d07644da251e2d3cc826f277b4634c47
ShiruOS <baligashireesh3@gmail.com>
2026-04-29 15:05:29 +0530

    fixed NA

--- src/announcer/main.py
+++ src/announcer/main.py
@@ -26,11 +26,6 @@ ids = DB.GetData("id")
 if content:
     for row in content:
         if row["id"] not in ids:
-            end_date = row["end"].split()[0]
-            end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
-            today = datetime.date.today()
-            if end_date < today:
-                continue
             DB.InsertData(row)
             ApiHandler.SendToDiscord(row, DISCORD_WEBHOOK_URL)
 
